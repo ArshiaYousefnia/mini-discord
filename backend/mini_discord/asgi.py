495c4f1b5ab655574eac5e7507c94cf5abdbf584
ArshiaYousefnia <arshia.yousefnia83@sharif.edu>
2026-06-09 17:41:32 +0330

    project structure and config initiated, deployment cicd config added.

--- backend/mini_discord/asgi.py
+++ backend/mini_discord/asgi.py
@@ -1,21 +1,16 @@
+"""
+ASGI config for mini_discord project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
 import os
+
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
 
-from users.routing import websocket_urlpatterns as user_websocket_urlpatterns
-from chat.routing import websocket_urlpatterns as chat_websocket_urlpatterns
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mini_discord.settings')
 
-
-combined_websocket_urlpatterns = user_websocket_urlpatterns + chat_websocket_urlpatterns
-
-from users.middleware import JWTWebsocketMiddleware
-
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": JWTWebsocketMiddleware(
-        AuthMiddlewareStack(
-            URLRouter(combined_websocket_urlpatterns)
-        )
-    ),
-})
+application = get_asgi_application()
