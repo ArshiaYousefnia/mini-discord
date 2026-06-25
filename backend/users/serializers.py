1e48d1dabed973d8e138042caceb6f017db8ab10
arshia.yousefnia83 <arshia.yousefnia83@sharif.edu>
2026-06-25 18:38:12 +0000

    #5: user registration backend

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,10 +1,8 @@
-
 from rest_framework import serializers
 from django.core.validators import EmailValidator, MinLengthValidator
 from django.core.exceptions import ValidationError
 
 from .fields import JalaliDateField
-from chat.models import Channel
 from .models import User
 import re
 
@@ -57,10 +55,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         """Validate username."""
         if len(value) < 4:
             raise serializers.ValidationError('Username must be at least 4 characters.')
-        if User.objects.filter(username__iexact=value).exists():
-            raise serializers.ValidationError('This username is already taken.')
-            
-        if Channel.objects.filter(public_id__iexact=value).exists():
+        if User.objects.filter(username=value).exists():
             raise serializers.ValidationError('This username is already taken.')
         return value
 
@@ -90,91 +85,3 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             display_name=validated_data['display_name']
         )
         return user
-    
-
-
-
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField(required=True)
-    password = serializers.CharField(
-        write_only=True,
-        required=True,
-        style={"input_type": "password"}
-    )
-
-
-class UserProfileSerializer(serializers.ModelSerializer):
-    avatar_url = serializers.CharField(read_only=True)
-
-    class Meta:
-        model = User
-        fields = (
-            'id',
-            'username',
-            'display_name',
-            'bio',
-            'avatar_url',
-            'is_online',
-        )
-
-        read_only_fields = fields
-
-
-
-
-class UserProfileUpdateSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(required=False)
-    display_name = serializers.CharField(required=False, max_length=32)
-    bio = serializers.CharField(required=False, allow_blank=True, max_length=190)
-    avatar = serializers.FileField(required=False)
-
-    class Meta:
-        model = User
-        fields = ("email", "display_name", "bio", "avatar", "username")
-        extra_kwargs = {
-            'username': {'read_only': True}
-        }
-
-    def validate_email(self, value):
-        EmailValidator()(value)
-
-        user = self.instance
-        if User.objects.exclude(id=user.id).filter(email=value).exists():
-            raise serializers.ValidationError("This email is already registered.")
-        return value
-
-    def validate_display_name(self, value):
-        if len(value) > 32:
-            raise serializers.ValidationError("Display name must be at most 32 characters.")
-        return value
-
-    def validate_bio(self, value):
-        if len(value) > 190:
-            raise serializers.ValidationError("Bio must be at most 190 characters.")
-        return value
-
-    def validate_avatar(self, value):
-        if value:
-            if value.size > 2 * 1024 * 1024:
-                raise serializers.ValidationError("Avatar must be smaller than 2MB.")
-
-            if not value.content_type in ["image/jpeg", "image/png"]:
-                raise serializers.ValidationError("Only JPG and PNG images are allowed.")
-
-        return value
-
-    def update(self, instance, validated_data):
-        return super().update(instance, validated_data)
-    
-
-class UserSearchSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = (
-            "id",
-            "username",
-            "display_name",
-            "bio",
-            "avatar_url",
-            "is_online",
-        )
