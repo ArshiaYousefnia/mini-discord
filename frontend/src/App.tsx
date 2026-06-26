57e97d08c332c53766565cbc9f1c4cad2b658395
Ali Alesheykhe <alesheykheali@gmail.com>
2026-06-27 03:20:36 +0330

    added routing to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
-import Register from "./pages/Register.tsx";
+import { Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
 
 function App() {
-  return <Register />;
+  return (
+    <Routes>
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
 }
 
 export default App;
