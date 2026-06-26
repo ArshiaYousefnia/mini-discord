4d0407f4d4e1bfbd4efacb949e3281f301e54cfc
nasim.javdani <nasim.javdani@gmail.com>
2026-06-26 14:36:52 +0000

    #7: user logout backend

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,10 +1,10 @@
 from rest_framework import generics, status
 from rest_framework.response import Response
-from .serializers import UserRegistrationSerializer
 from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import UserRegistrationSerializer, LoginSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class UserRegistrationView(generics.CreateAPIView):
     serializer_class = UserRegistrationSerializer
@@ -23,8 +23,9 @@ class UserRegistrationView(generics.CreateAPIView):
             },
             status=status.HTTP_201_CREATED
 
+
         )
-    
+
 
 class LoginView(APIView):
     def post(self, request):
@@ -53,3 +54,28 @@ class LoginView(APIView):
             },
             status=status.HTTP_200_OK
         )
+
+
+class LogoutView(APIView):
+
+    def post(self, request):
+        try:
+            refresh_token = request.data.get("refresh")
+            if not refresh_token:
+                return Response(
+                    {"error": "Refresh token is required."},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(
+                {"message": "Successfully logged out."},
+                status=status.HTTP_205_RESET_CONTENT
+            )
+        except Exception as e:
+            return Response(
+                {"error": str(e)},
+                status=status.HTTP_400_BAD_REQUEST
+            )
