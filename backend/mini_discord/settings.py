7f1a330e29d09981a2231bc4c1910c697467d85e
ArshiaYousefnia <arshia.yousefnia83@sharif.edu>
2026-06-26 19:03:21 +0330

    cicd: added unit tests for main branch

--- backend/mini_discord/settings.py
+++ backend/mini_discord/settings.py
@@ -29,10 +29,10 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY', "test_onlytest1276217ndsnkjdskajdsah")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG', default=False)
+DEBUG = env('DEBUG', default=True)
 
 ALLOWED_HOSTS = env('ALLOWED_HOSTS', default=[])
 
