1392ce240d31cc1a9bc2a5944ece919e1392d326
Ali Alesheykhe <alesheykheali@gmail.com>
2026-06-27 03:44:30 +0330

    fixed a register button bug

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 import { type RegisterPayload } from "../types/auth";
 
-const API_URL = import.meta.env.VITE_API_URL as string;
-
 export const registerUser = async (data: RegisterPayload) => {
-  const response = await axios.post(`${API_URL}/api/register/`, data);
+  const response = await axios.post("/api/register/", data);
   return response.data;
 };
-
