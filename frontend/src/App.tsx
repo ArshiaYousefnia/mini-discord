4615a7cec8082de448af2326ebec8545f08fd9e3
ali_alsheikh <ali_alsheikh@ce.sharif.edu>
2026-06-28 09:57:28 +0000

    #9: user profile frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import Register from "./pages/Register";
+import UserProfilePage from "./pages/UserProfilePage";
 
-function App() {
+export default function App() {
   return (
     <Routes>
+      <Route path="/" element={<h1>Home Works!</h1>} />
+      <Route path="/users/:userId" element={<UserProfilePage />} />
       <Route path="/register" element={<Register />} />
+      {/* Add your other routes here */}
     </Routes>
   );
 }
-
-export default App;
