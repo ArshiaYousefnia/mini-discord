05d085e9a9c82d85e51412181ba5d76eb0851006
ali_alsheikh <ali_alsheikh@ce.sharif.edu>
2026-06-28 09:57:28 +0000

    #9: user profile frontend

--- frontend/src/types/user.ts
+++ frontend/src/types/user.ts
@@ -4,7 +4,7 @@ export type BackendUserProfile = {
   display_name: string;
   bio?: string | null;
   avatar_url?: string | null;
-  is_online?: boolean;
+  is_online: boolean;
 };
 
 export type UserProfile = {
@@ -12,23 +12,6 @@ export type UserProfile = {
   username: string;
   display_name: string;
   bio?: string | null;
-  avatar_url: string;
+  avatar?: string | null;
   is_online: boolean;
 };
-
-export type UserEditProfile = {
-  username: string;
-  email: string;
-  display_name: string;
-  bio?: string | null;
-  avatar: string | null;
-};
-
-export interface UserSearchResult {
-  id: string;
-  username: string;
-  display_name: string;
-  bio: string;
-  avatar_url: string;
-  is_online?: boolean;
-}
