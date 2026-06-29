6573ebffd0b8f4dd963baa456f93bd28df0a15fc
ali_alsheikh <ali_alsheikh@ce.sharif.edu>
2026-06-29 18:14:06 +0000

    #6, #7: login and logout.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,41 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 import UserProfilePage from "./pages/UserProfilePage";
+import ProtectedRoute from "./components/ProtectedRoute";
+import EditProfilePage from "./pages/EditProfilePage";
+
+
+function Home() {
+  return <div>Home page</div>;
+}
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<h1>Home Works!</h1>} />
-      <Route path="/users/:userId" element={<UserProfilePage />} />
+      <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
-      {/* Add your other routes here */}
+      <Route path="/login" element={<Login />} />
+
+      <Route
+        path="/users/:userId"
+        element={
+          <ProtectedRoute>
+            <UserProfilePage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/users/:userId/profile/update/"
+        element={
+          <ProtectedRoute>
+            <EditProfilePage />
+          </ProtectedRoute>
+        }
+      />
+
+
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
--- frontend/src/services/users.ts
+++ frontend/src/services/users.ts
@@ -1,18 +1,35 @@
-import { apiRequest } from "./api";
-import type { UserProfile } from "../types/user";
-import type { BackendUserProfile } from "../types/user";
+import type { UserEditProfile, UserProfile } from "../types/user";
+import api from "./api";
 
-export async function getUserProfile(userId: string): Promise<UserProfile> {
-  const data = await apiRequest<BackendUserProfile>(
-    `/api/users/${userId}/profile/`
+
+
+export const getUserProfile = async (userId: string): Promise<UserProfile> => {
+  const response = await api.get(`/api/users/${userId}/profile/`);
+  return response.data;
+};
+
+export const getUserEditProfile = async (
+  userId: string
+): Promise<UserEditProfile> => {
+  const response = await api.get<UserEditProfile>(
+    `/api/users/${userId}/profile/update/`
   );
 
-  return {
-    id: data.id,
-    username: data.username,
-    display_name: data.display_name,
-    bio: data.bio,
-    avatar: data.avatar_url,
-    is_online: data.is_online,
-  };
-}
+  return response.data;
+};
+
+export const updateUserProfile = async (
+  userId: string,
+  formData: FormData
+): Promise<UserEditProfile> => {
+  const response = await api.patch<UserEditProfile>(
+    `/api/users/${userId}/profile/update/`,
+    formData
+  );
+
+  return response.data;
+};
+
+
+
+
