411131e7daadf462fb1be7014d09fdda513709d6
arshia.yousefnia83 <arshia.yousefnia83@sharif.edu>
2026-07-02 18:03:39 +0000

    feat: added api docs

--- backend/mini_discord/settings.py
+++ backend/mini_discord/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
+    "drf_spectacular",
     'users',
     'chat',
 ]
@@ -176,4 +177,12 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "My API",
+    "DESCRIPTION": "Backend API",
+    "VERSION": "1.0.0",
 }
