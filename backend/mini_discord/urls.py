321b5e19a2298986329a19b6e2e54106f44e70e1
arshia.yousefnia83 <arshia.yousefnia83@sharif.edu>
2026-07-02 18:03:39 +0000

    feat: added api docs

--- backend/mini_discord/urls.py
+++ backend/mini_discord/urls.py
@@ -20,6 +20,11 @@ from django.urls import path, include
 from users.views import UserProfileUpdateView
 from mini_discord import settings
 from users.views import UserRegistrationView, LoginView, LogoutView, UserProfileView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -33,6 +38,17 @@ urlpatterns = [
     name='user-profile-update'
     ),
     path('api/chat/', include('chat.urls')),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
 
 
