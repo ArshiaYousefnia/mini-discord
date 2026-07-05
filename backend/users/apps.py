3325c952e88b9baba80d8a4c918d2423538f4713
ArshiaYousefnia <arshia.yousefnia83@sharif.edu>
2026-07-05 20:34:53 +0330

    fixed profile retireval and storage errors

--- backend/users/apps.py
+++ backend/users/apps.py
@@ -1,3 +1,5 @@
+import sys
+
 from django.apps import AppConfig
 from django.core.files.storage import default_storage
 from storages.backends.s3boto3 import S3Boto3Storage
@@ -7,6 +9,5 @@ class UsersConfig(AppConfig):
     name = 'users'
 
     def ready(self):
-        # Replace the default storage with S3 (if it's not already)
-        if not isinstance(default_storage._wrapped, S3Boto3Storage):
+        if 'test' not in sys.argv and not isinstance(default_storage._wrapped, S3Boto3Storage):
             default_storage._wrapped = S3Boto3Storage()
