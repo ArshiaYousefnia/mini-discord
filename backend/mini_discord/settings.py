9191989b47bd10020cdbede7c121a3f32e089920
Nasim . <nasim.javdani@gmail.com>
2026-07-05 15:48:08 +0330

    fix profile photo

--- backend/mini_discord/settings.py
+++ backend/mini_discord/settings.py
@@ -155,23 +155,31 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
 AWS_ACCESS_KEY_ID = os.getenv('MINIO_ACCESS_KEY', 'minioadmin')
 AWS_SECRET_ACCESS_KEY = os.getenv('MINIO_SECRET_KEY', 'minioadmin')
 AWS_STORAGE_BUCKET_NAME = os.getenv('MINIO_BUCKET', 'avatars')
 AWS_S3_ENDPOINT_URL = 'http://minio:9000'
 AWS_S3_USE_SSL = False
 AWS_S3_VERIFY = False
-AWS_DEFAULT_ACL = 'public-read'
+AWS_DEFAULT_ACL = None
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_CUSTOM_DOMAIN = 'localhost:9000/avatars'
 AWS_AUTO_CREATE_BUCKET = True
 
-AWS_S3_URL_PROTOCOL = 'http'
+AWS_S3_URL_PROTOCOL = 'http:'
 
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
