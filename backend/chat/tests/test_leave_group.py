8b33ead4bdd93b6ea5e90ec6f5ffacdb8f0f5231
arshia.yousefnia83 <arshia.yousefnia83@sharif.edu>
2026-07-10 21:57:00 +0000

    #37, #15, #33: send message in group, leave group, search messages in a chat backend added

--- backend/chat/tests/test_leave_group.py
+++ backend/chat/tests/test_leave_group.py
@@ -2,10 +2,8 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
-from django.contrib.auth import get_user_model
+from users.models import User
 from chat.models import Conversation, ConversationMember
-
-User = get_user_model()
 
 class GroupLeaveTests(TestCase):
     def setUp(self):
