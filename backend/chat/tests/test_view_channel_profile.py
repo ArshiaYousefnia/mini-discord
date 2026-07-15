5c0348ea59add5dc83b0100c2f8aa8e03fca25eb
Nasim . <nasim.javdani@gmail.com>
2026-07-15 23:10:41 +0330

    Add channel profile view tests and endpoints

--- backend/chat/tests/test_view_channel_profile.py
+++ backend/chat/tests/test_view_channel_profile.py
@@ -2,7 +2,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from django.contrib.auth import get_user_model
-from .models import Conversation, Channel, ConversationMember, Role
+from chat.models import Conversation, Channel, ConversationMember, Role
 
 User = get_user_model()
 
