d160f0d8ba788c01c0c850dfa0e89e80a5bdd3a6
Nasim . <nasim.javdani@gmail.com>
2026-07-16 20:50:59 +0330

    Fix

--- backend/chat/serializers.py
+++ backend/chat/serializers.py
@@ -317,7 +317,8 @@ class ChannelDetailSerializer(serializers.ModelSerializer):
     owner_display_name = serializers.CharField(source="owner.display_name", read_only=True)
     avatar_url = serializers.SerializerMethodField()
     invite_link = serializers.SerializerMethodField() 
-    
+    is_private = serializers.BooleanField(source='channel.is_private', read_only=True)
+    public_id = serializers.CharField(source='channel.public_id', read_only=True)
     class Meta:
         model = Conversation
         fields = [
