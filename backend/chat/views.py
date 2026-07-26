dc08ebec967d1bcfd6b0e9dd74f72e2211d26d49
Nasim . <nasim.javdani@gmail.com>
2026-07-26 19:20:48 +0330

    Manage Other Topics

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -9,6 +9,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
+
 from .serializers import MessageSerializer, ConversationSerializer, ConversationMarkReadSerializer, \
     MinimalMessageSerializer, ChannelCreateSerializer, RoleSerializer, TopicSerializer, TopicUpdateSerializer, \
     TopicCreateSerializer, ChannelMessageSerializer
@@ -1227,6 +1228,8 @@ class TopicListCreateView(APIView):
         return Response(output.data, status=status.HTTP_201_CREATED)
 
 
+
+
 class TopicDetailView(APIView):
     permission_classes = [IsAuthenticated]
 
@@ -1247,8 +1250,14 @@ class TopicDetailView(APIView):
     def patch(self, request, conversation_id, topic_id):
         topic = self.get_topic(conversation_id, topic_id)
         member = ConversationMember.objects.get(conversation=topic.conversation, user=request.user)
-        if topic.creator != request.user and not member.roles.filter(can_manage_others_topics=True).exists():
-            raise PermissionDenied("You do not have permission to delete this topic.")
+        
+        is_owner = (topic.conversation.owner == request.user)
+        is_creator = (topic.creator == request.user)
+        can_manage = member.roles.filter(can_manage_others_topics=True).exists()
+
+        if not (is_owner or is_creator or can_manage):
+            raise PermissionDenied("You do not have permission to edit this topic.")
+
         serializer = TopicUpdateSerializer(topic, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         topic = serializer.save()
@@ -1257,11 +1266,17 @@ class TopicDetailView(APIView):
     def delete(self, request, conversation_id, topic_id):
         topic = self.get_topic(conversation_id, topic_id)
         member = ConversationMember.objects.get(conversation=topic.conversation, user=request.user)
-        if topic.creator != request.user and not member.roles.filter(can_manage_others_topics=True).exists():
+        
+        is_owner = (topic.conversation.owner == request.user)
+        is_creator = (topic.creator == request.user)
+        can_manage = member.roles.filter(can_manage_others_topics=True).exists()
+
+        if not (is_owner or is_creator or can_manage):
             raise PermissionDenied("You do not have permission to delete this topic.")
+            
         topic.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
+    
 class AttachmentDownloadView(APIView):
     permission_classes = [IsAuthenticated]
 
