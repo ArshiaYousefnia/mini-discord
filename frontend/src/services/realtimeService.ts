da8256231af04799c39bc87cf8c3b7c0402d5605
ali_alsheikh <alesheykheali@gmail.com>
2026-08-09 18:24:22 +0000

    #34, #82: message scheduling and some realtime bugfixes

--- frontend/src/services/realtimeService.ts
+++ frontend/src/services/realtimeService.ts
@@ -21,7 +21,9 @@ export interface ConversationUpdatePayload {
     | "unread_updated"
     | "conversation_deleted"
     | "group_updated"
-    | "user_updated";
+    | "user_updated"
+    | "member_removed"
+    | "conversation_metadata_updated";
   last_message?: MinimalMessage;
   user_id?: string;
   name?: string;
