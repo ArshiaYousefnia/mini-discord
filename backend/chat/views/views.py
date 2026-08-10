7d1e63b52622c936c6b1f184e7ba120bdd7de131
ArshiaYousefnia <arshia.yousefnia83@sharif.edu>
2026-08-10 14:16:56 +0330

    bugfix: realtime broadcasts

--- backend/chat/views/views.py
+++ backend/chat/views/views.py
@@ -18,8 +18,6 @@ from chat.views import (
     broadcast_new_message,
     broadcast_notification,
     broadcast_conversation_update,
-    broadcast_message_deleted,
-    broadcast_message_updated
 )
 
 from chat.serializers import (
